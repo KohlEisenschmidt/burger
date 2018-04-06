9b4186f0bb5f8ea94b3574d5ff2a743f7a4199f1
Kohl Eisenschmidt <kohleisenschmidt@gmail.com>
2018-04-05 20:26:56 -0400

    first push

--- db/schema.sql
+++ db/schema.sql
@@ -1,11 +1,14 @@
-### Schema
+DROP DATABASE IF EXISTS burger_db ;
 
-CREATE DATABASE cat_db;
-USE cat_db;
+-- ### Schema
 
-CREATE TABLE cats
+CREATE DATABASE burger_db;
+USE burger_db;
+
+CREATE TABLE burgers
 (
 	id int NOT NULL AUTO_INCREMENT,
-	name varchar(255) NOT NULL,
+	burger_name varchar(255) NOT NULL,
+	devoured BOOLEAN DEFAULT false,
 	PRIMARY KEY (id)
 );
---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+
+-- ### Seeds
+
+INSERT INTO burgers (burger_name) VALUES ('My favorite burger');
+INSERT INTO burgers (burger_name) VALUES ('Your favorite burger');

fe8ef25f0e0c5d1267e58380278516417d117765
Kohl Eisenschmidt <kohleisenschmidt@gmail.com>
2018-04-05 22:07:50 -0400

    got functionality down now need lots of css, and connect to heroku

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -17,9 +17,9 @@ router.get("/", function(req, res) {
 
   router.post("/api/burgers", function(req, res) {
     burger.create([
-      "name", "devoured"
+      "burger_name", "devoured"
     ], [
-      req.body.name, req.body.devoured
+      req.body.burger_name, req.body.devoured
     ], function(result) {
       // Send back the ID of the new quote
       res.json({ id: result.insertId });
@@ -30,9 +30,9 @@ router.put("/api/burgers/:id", function(req, res) {
     var condition = "id = " + req.params.id;
   
     console.log("condition", condition);
-  
+    console.log("request", req.body)
     burger.update({
-        devoured: req.body.devoured
+        devoured: req.body.devoured//----------------------------------------
     }, condition, function(result) {
       if (result.changedRows == 0) {
         // If no rows were changed, then the ID must not exist, so 404
